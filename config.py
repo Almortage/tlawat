29152e9e6e80222c1b701747e26efb6eefab8f4e
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-02-14 22:32:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ import os
 
 class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = os.environ.get("APP_ID", "")
 
     
